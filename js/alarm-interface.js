edd06d5050aca6594fc093892b1236a477464db9
lorenzo ferrario and paul hess <student@epicodus.com>
2016-03-15 12:54:35 -0700

    add snooze functionality

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -1,11 +1,19 @@
 var AlarmClock = require('./../js/AlarmClock.js').AlarmClock;
 $(document).ready(function(){
   alarmClock = new AlarmClock();
-  $('body').append('<div class="time">h</div>');
+  $('body').append('<div class="time"></div>');
+  $('#alarm').click(function(){
+    alarmClock.setAlarm($('#hour').val(), $('#minute').val());
+  });
   setInterval(function(){
     alarmClock.updateTime();
-    $('.time').text(alarmClock.time);
     alarmClock.checkForAlarms();
+    $('.time').text(alarmClock.time);
   } , 1000);
+  $('.snooze').click(function() {
+    alarmClock.snoozeAlarm();
+    $('div').remove('.cats');
+  });
+
 
 });
--- js/alarmClock.js
+++ js/alarmClock.js
@@ -9,17 +9,38 @@ AlarmClock.prototype.updateTime = function() {
       // return this.time;
 }
 
-AlarmClock.prototype.setAlarm = function(hour, minute, seconds) {
+AlarmClock.prototype.setAlarm = function(hour, minute) {
   var alarm = moment().set({'hour': hour, 'minute': minute, 'second': 0}).format('MMMM Do YYYY, h:mm:ss a');
   this.alarms.push(alarm);
 }
 
 AlarmClock.prototype.checkForAlarms = function() {
+  var self = this;
   for (var i = 0; i < this.alarms.length; i++) {
     if (this.time === this.alarms[i]) {
-      console.log('foo');
+      $('body').append('<div class="cats"></div>');
+      $('.cats').append('<img class="alarm-image" src="http://lorempixel.com/400/400/cats/">')
+      .append('<audio controls autoplay class="alarm-sound">' +
+                '<source src="./../assets/audio/meow.wav" type="audio/wav" alt="kitten meow">' +
+                 'Your browser does not support the audio element.' +
+                 '</audio>')
+      .append('<button class="shut-off">Shut Off</button>')
+      .append('<button class="snooze">snooze</button>');
+      this.alarms.splice(i, 1);
+      $('.shut-off').click(function() {
+      console.log('meow');
+      $('div').remove('.cats');
+      });
+      $('.snooze').click(function() {
+        self.snoozeAlarm();
+        $('div').remove('.cats');
+      });
     }
   }
+}
+
+AlarmClock.prototype.snoozeAlarm = function(){
+  this.setAlarm(moment().hour(), moment().add(1, 'm').minute());
 }
 
 exports.AlarmClock = AlarmClock;
