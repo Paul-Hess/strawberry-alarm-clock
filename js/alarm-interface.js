00df18784ec4fac5ace9eaed6c48478651b3bd7a
lorenzo ferrario and paul hess <student@epicodus.com>
2016-03-15 10:50:08 -0700

    add function alarm clock

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -1,10 +1,11 @@
 var AlarmClock = require('./../js/AlarmClock.js').AlarmClock;
 $(document).ready(function(){
   alarmClock = new AlarmClock();
-  $('body').append('<div class="time">hi</div>');
+  $('body').append('<div class="time">h</div>');
   setInterval(function(){
     alarmClock.updateTime();
     $('.time').text(alarmClock.time);
+    alarmClock.checkForAlarms();
   } , 1000);
 
 });
--- js/alarmClock.js
+++ js/alarmClock.js
@@ -1,11 +1,25 @@
 var moment = require('moment');
 var AlarmClock = function(){
   this.time;
+  this.alarms = [];
 }
 
 AlarmClock.prototype.updateTime = function() {
       this.time = moment().format('MMMM Do YYYY, h:mm:ss a');
       // return this.time;
+}
+
+AlarmClock.prototype.setAlarm = function(hour, minute, seconds) {
+  var alarm = moment().set({'hour': hour, 'minute': minute, 'second': 0}).format('MMMM Do YYYY, h:mm:ss a');
+  this.alarms.push(alarm);
+}
+
+AlarmClock.prototype.checkForAlarms = function() {
+  for (var i = 0; i < this.alarms.length; i++) {
+    if (this.time === this.alarms[i]) {
+      console.log('foo');
+    }
+  }
 }
 
 exports.AlarmClock = AlarmClock;
